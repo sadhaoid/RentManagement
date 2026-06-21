f97fdd99b1f2247dd51ee865886e7415cdd96a92
JunpengShu <1102237865@qq.com>
2026-06-22 00:36:05 +0800

    3rd phase

--- src/main/java/com/example/demo/utils/ThreadPoolPractice.java
+++ src/main/java/com/example/demo/utils/ThreadPoolPractice.java
@@ -5,12 +5,12 @@ import com.example.demo.entity.LeaseContractDo;
 import com.example.demo.reposity.LeaseContractRepository;
 import lombok.RequiredArgsConstructor;
 
-import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
+import java.util.concurrent.ConcurrentLinkedDeque;
+import java.util.concurrent.CountDownLatch;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
-import java.util.stream.Collectors;
 
 import org.apache.commons.collections4.ListUtils;
 
@@ -19,50 +19,35 @@ public class ThreadPoolPractice {
     private final LeaseContractRepository leaseContractRepository;
     LocalDateTime validateDate = LocalDateTime.of(2024, 6, 1, 0, 0, 0);
 
+    ConcurrentLinkedDeque<LeaseContractDo> concurrentCollection = new ConcurrentLinkedDeque<>();
 
-     void threadPool (String[] args) {
+    CountDownLatch countDownLatch = new CountDownLatch(4);
+
+
+     void threadPool () throws InterruptedException {
         // 创建一个固定大小的线程池，线程池大小为4
         ExecutorService executor = Executors.newFixedThreadPool(4);
 
-//        executor.submit(() -> {
-//            System.out.println("Task 1 is running in thread: " + Thread.currentThread().getName());
-//        });
-//
-//        executor.submit(() -> {
-//            System.out.println("Task 2 is running in thread: " + Thread.currentThread().getName());
-//        });
-//
-//        executor.submit(() -> {
-//            System.out.println("Task 3 is running in thread: " + Thread.currentThread().getName());
-//        });
-//
-//        executor.submit(() -> {
-//            System.out.println("Task 4 is running in thread: " + Thread.currentThread().getName());
-//        });
-//
-//        executor.submit(() -> {
-//            System.out.println("Task 5 is running in thread: " + Thread.currentThread().getName());
-//        });
-
         List<List<LeaseContractDo>> lists = partitionContract();
 
-        executor.submit(() -> {
-            lists.getFirst().stream().filter(leaseContractDo -> leaseContractDo.getEndDate().isAfter(validateDate)).toList();
-        });
+//        executor.submit(() -> {
+//            List<LeaseContractDo> list = lists.getFirst().stream().filter(leaseContractDo -> leaseContractDo.getEndDate().isAfter(validateDate)).toList();
+//            concurrentCollection.addAll(list);
+//            countDownLatch.countDown();
 
-         executor.submit(() -> {
-             lists.get(1).stream().filter(leaseContractDo -> leaseContractDo.getEndDate().isAfter(validateDate)).toList();
-         });
 
-         executor.submit(() -> {
-             lists.get(2).stream().filter(leaseContractDo -> leaseContractDo.getEndDate().isAfter(validateDate)).toList();
-         });
 
-         executor.submit(() -> {
-             lists.getLast().stream().filter(leaseContractDo -> leaseContractDo.getEndDate().isAfter(validateDate)).toList();
-         });
+         for (int i = 0; i < lists.size(); i++) {
+             int finalI = i;
+             executor.submit(() -> {
+                 lists.get(finalI).stream().filter(leaseContractDo -> leaseContractDo.getEndDate().isAfter(validateDate)).forEach(leaseContractDo -> concurrentCollection.add(leaseContractDo));
+                 countDownLatch.countDown();
+             });
+
+         }
 
         // 关闭线程池，等待所有任务完成后再关闭
+        countDownLatch.await();
         executor.shutdown();
     }
 
