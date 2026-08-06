2e1323a900f282a5c312678448104c86368f9eae
JunpengShu <1102237865@qq.com>
2026-08-07 02:43:40 +0800

    finish guard.txt

---
+++ src/main/java/com/example/demo/threadpool/ThreadPoolConfig.java
@@ -0,0 +1,25 @@
+package com.example.demo.threadpool;
+
+import jakarta.annotation.PreDestroy;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+import java.util.concurrent.LinkedBlockingQueue;
+import java.util.concurrent.ThreadPoolExecutor;
+import java.util.concurrent.TimeUnit;
+
+@Configuration
+public class ThreadPoolConfig {
+
+    @Bean
+    public ThreadPoolExecutor createThreadPool() {
+       return new ThreadPoolExecutor(4, 12, 100, TimeUnit.SECONDS, new LinkedBlockingQueue<>(6), new ThreadPoolExecutor.CallerRunsPolicy() );
+    }
+
+    @PreDestroy
+    public void shutdownThread() {
+        createThreadPool().shutdown();
+    }
+}
--- src/main/java/com/example/demo/utils/ThreadPoolPractice.java
+++ src/main/java/com/example/demo/utils/ThreadPoolPractice.java
@@ -3,6 +3,7 @@ package com.example.demo.utils;
 
 import com.example.demo.entity.LeaseContractDo;
 import com.example.demo.reposity.LeaseContractRepository;
+import jakarta.annotation.PreDestroy;
 import lombok.RequiredArgsConstructor;
 
 import java.time.LocalDateTime;
@@ -10,52 +11,39 @@ import java.util.List;
 import java.util.concurrent.*;
 
 import org.apache.commons.collections4.ListUtils;
+import org.springframework.stereotype.Component;
 
-//    public static ExecutorService newFixedThreadPool(int nThreads) {
-//        return new ThreadPoolExecutor(nThreads, nThreads,
-//                                      0L, TimeUnit.MILLISECONDS,
-//                                      new LinkedBlockingQueue<Runnable>());
-//    }
-
-
-//    public ThreadPoolExecutor(int corePoolSize,
-//                              int maximumPoolSize,
-//                              long keepAliveTime,
-//                              TimeUnit unit,
-//                              BlockingQueue<Runnable> workQueue) {
-//        this(corePoolSize, maximumPoolSize, keepAliveTime, unit, workQueue,
-//             Executors.defaultThreadFactory(), defaultHandler);
-//    }
+@Component
 @RequiredArgsConstructor
+
 public class ThreadPoolPractice {
     private final LeaseContractRepository leaseContractRepository;
+    private final ThreadPoolExecutor threadPoolExecutor;
+
+
     LocalDateTime validateDate = LocalDateTime.of(2024, 6, 1, 0, 0, 0);
 
     ConcurrentLinkedDeque<LeaseContractDo> concurrentCollection = new ConcurrentLinkedDeque<>();
 
-    CountDownLatch countDownLatch = new CountDownLatch(4);
 
-
-     void threadPool () throws InterruptedException {
+     void threadPool() throws InterruptedException {
         // 创建一个固定大小的线程池，线程池大小为4
-        ExecutorService executor = Executors.newFixedThreadPool(4);
 
-//        new ThreadPoolExecutor(4, 12, 100000, TimeUnit.SECONDS, List<>(),)
+        CountDownLatch countDownLatch = new CountDownLatch(partitionContract().size());
+
 
         List<List<LeaseContractDo>> lists = partitionContract();
 
          for (int i = 0; i < lists.size(); i++) {
              int finalI = i;
-             executor.submit(() -> {
+             threadPoolExecutor.submit(() -> {
                  lists.get(finalI).stream().filter(leaseContractDo -> leaseContractDo.getEndDate().isAfter(validateDate)).forEach(leaseContractDo -> concurrentCollection.add(leaseContractDo));
                  countDownLatch.countDown();
              });
 
          }
 
-        // 关闭线程池，等待所有任务完成后再关闭
         countDownLatch.await();
-        executor.shutdown();
     }
 
     public  List<List<LeaseContractDo>> partitionContract() {
